1daf3bf58369f7272ec255b59d6647d3d7f3c2b6
MMusembei <meshnick01@gmail.com>
2022-06-30 14:07:31 -0700

    Extra c question

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,16 @@
 #include <unistd.h>
 
 /**
- * main - Prints a specific string given
- * followed by a newline to the standard error
- * Return: Correctly executed return 1
+ * main - prints a specific phrase given
+ * followed by a new line to the standard error
+ * Return: 0 when (successful)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+
 }
+
+
