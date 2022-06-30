2dffe14d2ac644f9da827f6e1293716825aa1480
MMusembei <meshnick01@gmail.com>
2022-06-30 12:58:49 -0700

    Using the printf fxn to print a given phrase

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - using the printf fxn, a phrase given is printed
+ * Return: 0 if excuted perfectly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
