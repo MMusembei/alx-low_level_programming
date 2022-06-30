eb45cb933944bfa8046dbe239f9a609e6beefc5d
MMusembei <meshnick01@gmail.com>
2022-06-30 12:21:51 -0700

    Use the puts function to print a string and return 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- * Use the puts function to print out an exact statement given
- * After the output, print a newline
- * Program should end with value 0
+ * Use the puts function - to print out an exact statement given
+ *
+ * After the output, print a newlin
+ *
+ *Return: Program should end with value 0
  */
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return(0);
 }
