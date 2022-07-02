38eedaf5a74319bab422bbfd2e82fda672ef37d1
MMusembei <meshnick01@gmail.com>
2022-07-01 18:02:06 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all base single digits starting from 0
+ * Return: 0 when executed
+ */
+
+int main(void)
+{
+	int i;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
