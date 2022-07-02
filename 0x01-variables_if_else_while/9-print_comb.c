cb68aa1463ec86a065f75df06ce8ffe60096cd8c
MMusembei <meshnick01@gmail.com>
2022-07-01 18:34:52 -0700

    task 9"

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all the combinations of a digit separated by commas and space
+ * Return: 0 when executed
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		putvhar(j);
+		if  (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
