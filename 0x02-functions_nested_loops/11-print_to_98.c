67ccaea4871b5c5a449b518dfbd0520c8b6918fe
MMusembei <meshnick01@gmail.com>
2022-07-06 17:05:18 -0700

    task 11 for final mandatory

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,10 +3,11 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- * The print a n.
+ * Then print a newline
+ * @n: print this int
  */
 
-void print_to_98(void)
+void print_to_98(int n)
 {
 	int i, j;
 
