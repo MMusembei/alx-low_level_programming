07584d741d6c9d7511616cc1eab898ef28775b43
MMusembei <meshnick01@gmail.com>
2022-07-11 15:07:06 -0700

    Print out char 1 of 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints 1 char out of 2
+ * then a newline
+ * #str: str to print the chars from
+ */
+void puts2(char *str)
+{
+	int j, len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (j = 0; j < len; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
+
