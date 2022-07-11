807ec048c429df55439c04e3dd41b3946c3dda13
MMusembei <meshnick01@gmail.com>
2022-07-11 16:18:42 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,18 +14,16 @@ char *_strcpy(char *dest, char *src);
 	int len, j;
 	 
 	len = 0;
-
-	while (src[len] != '\0')
+	while (*(src + len))
 	{
 		len++;
 	}
-	
 	for (j = 0; j < len; j++)
 	{
-		dest[j] = src[j];
+		*(dest + j) = *(src + j);
 	}
 
-	dest[j] = '\0';
+	*(dest + j) = '\0';
 
 	return (dest);
 }
