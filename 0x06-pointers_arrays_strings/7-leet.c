6ebdf4985cfe4392c44d4d51797f04c4b729ad67
MMusembei <meshnick01@gmail.com>
2022-07-15 03:56:16 -0700

    leet 2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,19 +8,16 @@
 char *leet(char *n)
 {
 	int i, j;
-
 	char s1[10] = "aAeEoOtT1L";
 	char s2[10] = "4433007711";
-	
+
 	i = 0;
 	while (n[i])
 	{
 		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == s1[j])
-			{
 				n[i] = s2[j];
-			}
 		}
 	i++;
 	}
