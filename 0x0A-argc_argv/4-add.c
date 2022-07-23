f8d7c2d1561672d460459e1503749f85f4de03ce
MMusembei <meshnick01@gmail.com>
2022-07-23 15:08:45 -0700

    Write a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of arguements
+ * @argc: arguement count
+ * @argv: array of pointers to CLI arguement
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+
+if (argc == 1)
+{
+	printf("o\n");
+	return (0);
+}
+while (--argc)
+{
+	if (*argv[argc] >= 'a' && *argv[argc] <= 'z')
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+		sum += (atoi(argv[argc]));
+}
+printf("%d\n", sum);
+return (0);
+}
