c0f11840db3378ae8677a07afbe65e20651e09e9
MMusembei <meshnick01@gmail.com>
2022-08-02 09:29:48 -0700

    task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stddef.h>
 
 /**
  * struct dog - a dog's basic info
@@ -26,6 +27,5 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
